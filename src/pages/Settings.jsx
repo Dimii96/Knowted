6f2b0430c6b67b216a4106e9e0f1ed73c934efcc
Dimitri <mich_dkara1@hotmail.com>
2021-11-11 16:29:11 +1000

    Start implementing some accessibility options

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -5,6 +5,7 @@ function Settings() {
   return (
     <div className="container">
       <div id="page-title">Settings</div>
+
       <div className="section">
         <div className="section-content">
           <div className="section-title subheading px-2">
@@ -16,6 +17,56 @@ function Settings() {
           </div>
         </div>
       </div>
+
+      <div className="section" hidden>
+        <div className="section-content">
+          <div className="section-title subheading px-2">
+            <label>Accessibility (Not functional yet)</label>
+          </div>
+          <div className="section-main">
+
+            <div className="row  px-0">
+              <div className="col-12">
+                <label className="form-label bold">Font Size: 12px</label>
+              </div>
+              <div className="col-12 col-sm-8 col-md-8 col-lg-6">
+                <input type="range" className="form-range " min="0" max="5" defaultValue="3" id="customRange2" />
+              </div>
+
+            </div>
+
+            <div className="row px-0">
+              <div className="col-12">
+                <label className="form-label bold">Theme</label>
+              </div>
+              <div className="col-12 col-sm-8 col-md-8 col-lg-6">
+                <div className="form-check">
+                  <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" defaultChecked/>
+                  <label className ="form-check-label" htmlFor="exampleRadios1">
+                  Light
+                  </label>
+                </div>
+                <div className="form-check">
+                  <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2"/>
+                  <label className ="form-check-label" htmlFor="exampleRadios2">
+                  Dark
+                  </label>
+                </div>
+                <div className="form-check">
+                  <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option3" disabled/>
+                  <label className ="form-check-label" htmlFor="exampleRadios3">
+                  Other
+                  </label>
+                </div>
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+
     </div>
   );
 }
