b59055e2744e134b64949cf751d4966ee7172364
Dimitri <mich_dkara1@hotmail.com>
2022-03-24 17:09:29 +1000

    Add argument to pass through parameters for SQL injection prevention

--- src/message-control/main.js
+++ src/message-control/main.js
@@ -5,10 +5,11 @@ const database = new sqlite3.Database('./src/db.sqlite3', sqlite3.OPEN_READWRITE
     if (err) console.error('Database opening error: ', err);
 });
 
-ipcMain.on('asynchronous-message', (event, arg) => {
+ipcMain.on('asynchronous-message', (event, sqlQuery, parameters) => {
     try {
-        const sql = arg;
-        database.all(sql, (err, rows) => {
+        //const sql = arg;
+        console.log("parameters: ", parameters)
+        database.all(sqlQuery, parameters, (err, rows) => {
             event.reply('asynchronous-reply', (err && err.message) || rows);
         });
 
